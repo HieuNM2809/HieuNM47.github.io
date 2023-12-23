4e771a11324d2d15b8560e5ffaf7f600301d04c9
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2023-12-23 15:23:23 +0700

    a

---
+++ app-android/essets/script.js
@@ -0,0 +1,147 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBckkNHIvMoIgxIHt_3HOTlMcso2V2uN_Q",
+    authDomain: "database-app-android-4c845.firebaseapp.com",
+    databaseURL: "https://database-app-android-4c845-default-rtdb.firebaseio.com",
+    projectId: "database-app-android-4c845",
+    storageBucket: "database-app-android-4c845.appspot.com",
+    messagingSenderId: "596263588445",
+    appId: "1:596263588445:web:ef8e05eee6120f71eeb3b4"
+};
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Lấy reference đến Firebase Realtime Database
+var database = firebase.database();
+
+var temperatureKey = "setting";
+database.ref(temperatureKey).on('value', function (snapshot) {
+    var data = snapshot.val();
+
+    console.log('data', data);
+    applyButtonStates(data);
+});
+
+$(document).ready(function () {
+    // Hiển thị loading khi trang được tải
+    var containerLoading = $('.container-loading');
+    var containerContent = $('.container-content');
+
+    // Thực hiện gọi API bằng AJAX
+    setTimeout(() => {
+        $.ajax({
+            url: 'https://database-app-android-4c845-default-rtdb.firebaseio.com/setting.json',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+
+                applyButtonStates(data);
+
+                // Ẩn loading khi dữ liệu đã được tải
+                containerLoading.hide();
+                containerContent.show();
+            },
+            error: function (error) {
+                // Xử lý lỗi
+                console.error('Error:', error);
+                Swal.fire({
+                    position: "center",
+                    icon: "warning",
+                    title: "Kết nối database thất bại, vui lòng thử lại !",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+                // Ẩn loading khi có lỗi xảy ra
+                containerLoading.hide();
+                containerContent.show();
+            }
+        });
+    }, 1000);
+});
+function toggleButton(button) {
+    // Kiểm tra xem button có lớp bật hay không không
+    var isOn = button.classList.contains('turn-on');
+    var id = $('#' + button.id);
+    var message = "Hệ thống đang bảo trì, vui lòng thử lại sau!";
+
+    // bật 
+    if (!isOn) {
+        button.classList.add('turn-on');
+        id.attr("value", true);
+        message = "Bật cảnh báo " + id.attr("text-title") + " thành công";
+    } else {
+        button.classList.remove('turn-on');
+        id.attr("value", false);
+        message = "Tắt cảnh báo " + id.attr("text-title") + " thành công";
+    }
+    getAllValuesAndCallApi(message);
+}
+function applyButtonStates(data) {
+    // Duyệt qua các key trong data và áp dụng trạng thái lên các button
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            var buttonId = '#' + key;
+            var button = $(buttonId);
+
+            // Kiểm tra giá trị của key và thêm/loại bỏ lớp turn-on
+            if (data[key] === "true") {
+                button.addClass('turn-on');
+            } else {
+                button.removeClass('turn-on');
+            }
+            if (key === "temperature") {
+                $('#temperature').html(`NHIỆT ĐỘ: ${data.temperature} độ C`);
+            }
+            $('#' + key).attr('value', data[key]);
+        }
+    }
+}
+function getAllValues() {
+    var values = {};
+
+    // Duyệt qua mảng các ID và lấy giá trị từ các thẻ tương ứng
+    var ids = [
+        'area1', 'area2', 'area3', 'area4', 'area5',
+        'area6', 'area7', 'area8', 'sos', 'alert', 'temperature'
+    ];
+
+    for (var i = 0; i < ids.length; i++) {
+        var id = ids[i];
+        var value = $('#' + id).attr("value");
+        values[id] = value;
+    }
+
+    return values;
+}
+function getAllValuesAndCallApi(message) {
+    // Lấy giá trị từ tất cả các thẻ button
+    var values = getAllValues();
+
+    // Gọi API để cập nhật dữ liệu trên server
+    $.ajax({
+        url: 'https://database-app-android-4c845-default-rtdb.firebaseio.com/setting.json',
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(values),
+        success: function (response) {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: message,
+                showConfirmButton: false,
+                timer: 1500
+            });
+        },
+        error: function (error) {
+            console.error('Error updating data:', error);
+            Swal.fire({
+                position: "center",
+                icon: "warning",
+                title: JSON.stringify(error),
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+    });
+}
+
---
+++ app-android/essets/scriptLogin.js
@@ -0,0 +1,87 @@
+
+var inP = $(".input-field");
+$(document).ready(function () {
+    // Hiển thị loading khi trang được tải
+    var containerLoading = $('.container-loading');
+    var containerContent = $('.container-content');
+
+    // Thực hiện gọi API bằng AJAX
+    setTimeout(() => {
+        containerLoading.hide();
+        containerContent.show();
+    }, 1000);
+});
+
+inP
+    .on("blur", function () {
+        if (!this.value) {
+            $(this).parent(".f_row").removeClass("focus");
+        } else {
+            $(this).parent(".f_row").addClass("focus");
+        }
+    })
+    .on("focus", function () {
+        $(this).parent(".f_row").addClass("focus");
+        $(".btn").removeClass("active");
+        $(".f_row").removeClass("shake");
+    });
+
+$(".resetTag").click(function (e) {
+    e.preventDefault();
+    $(".formBox").addClass("level-forget").removeClass("level-reg");
+});
+
+$(".back").click(function (e) {
+    e.preventDefault();
+    $(".formBox").removeClass("level-forget").addClass("level-login");
+});
+
+$(".regTag").click(function (e) {
+    e.preventDefault();
+    $(".formBox").removeClass("level-reg-revers");
+    $(".formBox").toggleClass("level-login").toggleClass("level-reg");
+    if (!$(".formBox").hasClass("level-reg")) {
+        $(".formBox").addClass("level-reg-revers");
+    }
+});
+$(".btn-confirm").each(function () {
+  
+   
+    $(this).on("click", function (e) {
+        e.preventDefault();
+
+        var userName = $('#username').val();
+        var password = $('#password').val;
+
+        var finp = $(this).parent("form").find("input");
+
+
+        if (!finp.val() == 0) {
+            console.log(userName, password);
+            if (userName == 'admin' && password == "123456") {
+                $(this).addClass("active");
+                setTimeout(function () {
+                    window.location.href = '/';
+                }, 2000);
+            }
+        }
+
+        inP.val("");
+
+        $(".f_row").removeClass("shake focus");
+        $(".btn").removeClass("active");
+
+        if (inP.val() == 0) {
+            inP.parent(".f_row").addClass("shake");
+        }
+
+        
+
+        //inP.val('');
+        //$('.f_row').removeClass('focus');
+    });
+
+    
+    
+});
+
